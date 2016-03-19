b420d090d59bea97e936c531e28909d8b4762d49
Bill Wilder <bill.wilder@finomial.com>
2016-03-19 15:30:04 -0400

    change twilio accounts

--- SportRadarPlayByPlay/SportRadarPlayByPlay/Alerter.cs
+++ SportRadarPlayByPlay/SportRadarPlayByPlay/Alerter.cs
@@ -6,11 +6,16 @@ namespace SportRadarPlayByPlay
 {
     public class Alerter
     {
+#if false
         const string TwilioAccountSid = "AC6d2473613c874057901961332489861d";
         const string TwilioAuthToken = "83252b3397448cf61693419440122ee8";
         const string TwilioSourcePhoneNumberSid = "PN8cbfc7f39bacccb03ad7bca48ba35591";
         const string TwilioPhoneNumber = "+17812096258";
-
+#else
+        const string TwilioAccountSid = "ACcbf6661cd3bc9a63c2f3bc5a38f87b82";
+        const string TwilioAuthToken = "c8d0cbf8a84329d3ed67fa6d43ef8205";
+        const string TwilioPhoneNumber = "+17813814076";
+#endif
         private static List<string> GetDestinationPhoneNumbers()
         {
             var phoneNumbers = new List<string>();
